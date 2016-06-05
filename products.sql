b19dc883669cf52c21c4ad6057e02884160c308b
Martín R <martin@estudio12.com.ar>
2016-06-05 15:43:55 -0300

    Create products.sql

---
+++ products.sql
@@ -0,0 +1,58 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+TRUNCATE TABLE `catalog_product_bundle_option`;
+TRUNCATE TABLE `catalog_product_bundle_option_value`;
+TRUNCATE TABLE `catalog_product_bundle_selection`;
+TRUNCATE TABLE `catalog_product_entity_datetime`;
+TRUNCATE TABLE `catalog_product_entity_decimal`;
+TRUNCATE TABLE `catalog_product_entity_gallery`;
+TRUNCATE TABLE `catalog_product_entity_int`;
+TRUNCATE TABLE `catalog_product_entity_media_gallery`;
+TRUNCATE TABLE `catalog_product_entity_media_gallery_value`;
+TRUNCATE TABLE `catalog_product_entity_text`;
+TRUNCATE TABLE `catalog_product_entity_tier_price`;
+TRUNCATE TABLE `catalog_product_entity_varchar`;
+TRUNCATE TABLE `catalog_product_link`;
+TRUNCATE TABLE `catalog_product_link_attribute`;
+TRUNCATE TABLE `catalog_product_link_attribute_decimal`;
+TRUNCATE TABLE `catalog_product_link_attribute_int`;
+TRUNCATE TABLE `catalog_product_link_attribute_varchar`;
+TRUNCATE TABLE `catalog_product_link_type`;
+TRUNCATE TABLE `catalog_product_option`;
+TRUNCATE TABLE `catalog_product_option_price`;
+TRUNCATE TABLE `catalog_product_option_title`;
+TRUNCATE TABLE `catalog_product_option_type_price`;
+TRUNCATE TABLE `catalog_product_option_type_title`;
+TRUNCATE TABLE `catalog_product_option_type_value`;
+TRUNCATE TABLE `catalog_product_super_attribute`;
+TRUNCATE TABLE `catalog_product_super_attribute_label`;
+TRUNCATE TABLE `catalog_product_super_attribute_pricing`;
+TRUNCATE TABLE `catalog_product_super_link`;
+TRUNCATE TABLE `catalog_product_enabled_index`;
+TRUNCATE TABLE `catalog_product_website`;
+TRUNCATE TABLE `catalog_product_entity`;
+TRUNCATE TABLE `cataloginventory_stock_item`;
+TRUNCATE TABLE `cataloginventory_stock_status`;
+TRUNCATE TABLE `catalog_category_entity`;
+TRUNCATE TABLE `catalog_category_entity_datetime`;
+TRUNCATE TABLE `catalog_category_entity_decimal`;
+TRUNCATE TABLE `catalog_category_entity_int`;
+TRUNCATE TABLE `catalog_category_entity_text`;
+TRUNCATE TABLE `catalog_category_entity_varchar`;
+TRUNCATE TABLE `catalog_category_product`;
+TRUNCATE TABLE `catalog_category_product_index`;
+TRUNCATE TABLE `catalog_product_relation`;
+TRUNCATE TABLE `catalog_product_flat_1`;
+TRUNCATE TABLE `catalog_category_flat_store_1`;
+TRUNCATE TABLE `catalog_category_flat_store_2`;
+TRUNCATE TABLE `catalog_category_flat_store_3`;
+
+-- Tags
+TRUNCATE `tag`;
+TRUNCATE `tag_relation`;
+TRUNCATE `tag_summary`;
+ALTER TABLE `tag` AUTO_INCREMENT=1;
+ALTER TABLE `tag_relation` AUTO_INCREMENT=1;
+ALTER TABLE `tag_summary` AUTO_INCREMENT=1;
+
+SET FOREIGN_KEY_CHECKS = 1;
